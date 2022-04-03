771dc25313c4160f2124d933c09932b4ba6e3d51
Guillaume Vernade <guillaume.vernade__github@normalesup.org>
2022-04-03 03:52:36 -0400

    HA/HACS update

--- custom_components/myEnedis/myCall.py
+++ custom_components/myEnedis/myCall.py
@@ -10,18 +10,61 @@ except ImportError:
         __nameMyEnedis__,
     )
 
+import datetime
 import logging
+import re
+import sys
 
-log = logging.getLogger(__nameMyEnedis__)
-waitCall = 1  # 1 secondes
+_LOGGER = logging.getLogger(__nameMyEnedis__)
+
+# Min and Max Delays for initial call and subsequent calls.
+# The purpose is to make some random distribution between client calls
+# so that clients do not sollicit the gateway at the sime time.
+INITIAL_CALL_DELAY = 1.0  # Minimum time to wait for first call
+NEXT_MIN_CALL_DELAY = 55.0  # Minimum Time to wait for second call and next calls
+MAX_CALL_DELAY = 125.0  # Maximum Time to wait for call
+
+if any(re.findall(r"pytest|py.test|testEnedis", sys.argv[0])):
+    # Shorter delays during test
+    INITIAL_CALL_DELAY = 0.250
+    NEXT_MIN_CALL_DELAY = 0.5
+    MAX_CALL_DELAY = 1.0
+
+MAX_CALLS = 50
+# In seconds - delay required since previous timeout to try a new call
+MAX_PREVIOUS_TIMEOUT = 3600
 
 
 class myCall:
+    _MyCallsSinceRestart = 0
+    _MyCallsUpdateDay = ""
+    _lastTimeout = 0.0
+    _noRecentTimeout = True
+
     def __init__(self):
         self._lastAnswer = None
         self._contentType = "application/json"
         self._contentHeaderMyEnedis = "home-assistant-myEnedis"
         self._serverName = "https://enedisgateway.tech/api"
+
+    @staticmethod
+    def sanitizeCounter() -> int:
+        # Check if new day to reset the counter
+        day = datetime.date.today().strftime("%Y-%m-%d")
+        if day != myCall._MyCallsUpdateDay:
+            # New day, reset counter
+            myCall._MyCallsSinceRestart = 0
+            myCall._MyCallsUpdateDay = day
+
+        return myCall._MyCallsSinceRestart
+
+    @staticmethod
+    def increaseCallCounter() -> int:
+        """Increase an get the number of service calls"""
+        myCall.sanitizeCounter()
+
+        myCall._MyCallsSinceRestart += 1
+        return myCall._MyCallsSinceRestart
 
     def setParam(self, PDL_ID: str, token: str, version: str):
         self._PDL_ID, self._token, self._version = PDL_ID, token, version
@@ -40,24 +83,86 @@ class myCall:
     def getLastAnswer(self):
         return self._lastAnswer
 
+    @staticmethod
+    def isAvailable() -> bool:
+        """Return true if the service is available"""
+        # The serice is considered not available when:
+        # - There were too many recent Timeouts
+        # - The number of daily requests is exceeded
+        if myCall.sanitizeCounter() >= MAX_CALLS:
+            _LOGGER.debug("Nombre d'appels maximum journalier dépassé")
+            return False
+
+        if not myCall._noRecentTimeout:
+            timestamp = datetime.datetime.now().timestamp()
+            _LOGGER.debug(
+                f"Ancien timeout? {timestamp}-{myCall._lastTimeout}"
+                f"={timestamp-myCall._lastTimeout}"
+                f"> {MAX_PREVIOUS_TIMEOUT} ?"
+            )
+            if timestamp - myCall._lastTimeout > MAX_PREVIOUS_TIMEOUT:
+                myCall._noRecentTimeout = True
+
+        return myCall._noRecentTimeout
+
+    @staticmethod
+    def handleTimeout():
+        timestamp = datetime.datetime.now().timestamp()
+        if timestamp - myCall._lastTimeout < MAX_PREVIOUS_TIMEOUT:
+            myCall._noRecentTimeout = False
+
+        myCall._lastTimeout = timestamp
+
+    def saveApiReturn(self, idx: int, data: str):
+        """Save return from API to index file to produce test data"""
+        import os
+
+        fname = os.path.dirname(__file__) + f"/myEnedis/test_data/data_{idx}.txt"
+        os.makedirs(os.path.dirname(fname), exist_ok=True)
+        with open(fname, "w") as f:
+            f.write(data)
+
     def post_and_get_json(self, url, params=None, data=None, headers=None):
-        try_again = True
-        nbEssai = 0
+        import json
+        import random
+        import time
+
+        import requests
+
+        maxTriesToGo: int = 4  # Number of gateway requests that can be made
         dataAnswer = None
-        while try_again:
-            nbEssai += 1
+        minDelay: float = INITIAL_CALL_DELAY
+        while maxTriesToGo > 0:
+            maxTriesToGo -= 1
             try:
-                import time
+                if not myCall.isAvailable():
+                    dataAnswer = {
+                        "error_code": "UNAVAILABLE",
+                        "enedis_return": {
+                            "error": "UNAVAILABLE",
+                            "message": "Indisponible, essayez plus tard",
+                        },
+                    }
+                    self.setLastAnswer(dataAnswer)
+                    maxTriesToGo = 0
+                    continue  # Next loop, so exit.
 
-                time.sleep(waitCall)
-                import json
-
-                import requests
+                # Wait some random time so that clients are not making requests
+                # in the same second.
+                sleepDuration = random.uniform(minDelay, MAX_CALL_DELAY)
+                _LOGGER.debug(f"Sleeping {sleepDuration}s")
+                time.sleep(sleepDuration)
+                minDelay = NEXT_MIN_CALL_DELAY
 
                 session = requests.Session()
                 session.verify = True
-                # print("ici", params, headers, data)
-                log.info("====== Appel http !!! =====")
+
+                # Set up log prefix, with counter
+                counter = myCall.increaseCallCounter()
+                logPrefix = f"====== Appel http #{counter} !!! "
+                _LOGGER.info(f"{logPrefix}=====")
+
+                # raise(requests.exceptions.Timeout) # pour raiser un timeout de test ;)
                 response = session.post(
                     url,
                     params=params,
@@ -65,17 +170,20 @@ class myCall:
                     headers=headers,
                     timeout=30,
                 )
+                # Generate test data with next line
+                self.saveApiReturn(counter, response.text)
+
                 response.raise_for_status()
                 dataAnswer = response.json()
                 self.setLastAnswer(dataAnswer)
-                log.info("====== Appel http !!! headers : %s =====" % (headers))
-                log.info("====== Appel http !!! data : %s =====" % (data))
-                log.info("====== Appel http !!! reponse : %s =====" % (dataAnswer))
-                # raise(requests.exceptions.Timeout) # pour raiser un timeout de test ;)
-                try_again = False
-            except requests.exceptions.Timeout as error:
+                _LOGGER.info(f"{logPrefix}headers : {headers} =====")
+                _LOGGER.info(f"{logPrefix}data : {data} =====")
+                _LOGGER.info(f"{logPrefix}reponse : {dataAnswer} =====")
+                maxTriesToGo = 0  # Done
+            except requests.exceptions.Timeout:
+                myCall.handleTimeout()
                 # a ajouter raison de l'erreur !!!
-                log.error("====== Appel http !!! requests.exceptions.Timeout")
+                _LOGGER.error(f"{logPrefix}requests.exceptions.Timeout")
                 dataAnswer = {
                     "enedis_return": {
                         "error": "UNKERROR_TIMEOUT",
@@ -83,29 +191,31 @@ class myCall:
                     }
                 }
                 self.setLastAnswer(dataAnswer)
-            except requests.exceptions.HTTPError as error:
-                log.info("====== Appel http !!! requests.exceptions.HTTPError")
+            except requests.exceptions.HTTPError:
+                _LOGGER.error(f"{logPrefix}requests.exceptions.HTTPError")
                 if ("ADAM-ERR0069" not in response.text) and (
                     "__token_refresh_401" not in response.text
                 ):
-                    log.error("*" * 60)
-                    log.error(f"header : {headers} ")
-                    log.error(f"params : {params} ")
-                    log.error(f"data : {json.dumps(data)} ")
-                    log.error(f"Error JSON : {response.text} ")
-                    log.error("*" * 60)
+                    _LOGGER.error("*" * 60)
+                    _LOGGER.error(f"header : {headers} ")
+                    _LOGGER.error(f"params : {params} ")
+                    _LOGGER.error(f"data : {json.dumps(data)} ")
+                    _LOGGER.error(f"Error JSON : {response.text} ")
+                    _LOGGER.error("*" * 60)
                 # with open('error.json', 'w') as outfile:
                 #    json.dump(response.json(), outfile)
                 dataAnswer = response.json()
                 self.setLastAnswer(dataAnswer)
-                try_again = False
-                if "usage_point_id parameter must be 14 digits long." in response.text:
-                    try_again = True  # si le nombre de digit n'est pas de 14 ...lié à une erreur coté enedis
-            if (try_again) and (nbEssai > 2):
-                import time
 
-                time.sleep(30)  # on attend quelques secondes
-                try_again = False
+                # Ne pas retenter l'appel,
+                # Sauf si erreur côté enedis ("si le nombre de digit n'est pas de 14"),
+                if (
+                    "usage_point_id parameter must be 14 digits long."
+                    not in response.text
+                ):
+                    maxTriesToGo = 0  # Fatal error, do not try again
+
+        _LOGGER.debug("Data answer: %r", dataAnswer)
         # if ( "enedis_return" in dataAnswer.keys() ):
         #    if ( type( dataAnswer["enedis_return"] ) is dict ):
         #        if ( "error" in dataAnswer["enedis_return"].keys()):
@@ -115,7 +225,7 @@ class myCall:
 
     def getDataPeriod(self, deb: str, fin: str | None) -> tuple[str, bool]:
         if fin is not None:
-            log.info(f"--get dataPeriod : {deb} => {fin} --")
+            _LOGGER.info(f"--get dataPeriod : {deb} => {fin} --")
             payload = {
                 "type": "daily_consumption",
                 "usage_point_id": self._PDL_ID,
@@ -136,7 +246,7 @@ class myCall:
 
     def getDataPeriodConsumptionMaxPower(self, deb, fin):
         if fin is not None:
-            log.info(f"--get dataPeriod : {deb} => {fin} --")
+            _LOGGER.info(f"--get dataPeriod : {deb} => {fin} --")
             payload = {
                 "type": "daily_consumption_max_power",
                 "usage_point_id": self._PDL_ID,
